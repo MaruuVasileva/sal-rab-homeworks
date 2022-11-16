7bac023831b2b3d7e94a9f36c4149758a72f0991
MaruuVasileva <maruu.vasileva@gmail.com>
2022-11-16 12:28:26 +0400

    HW4

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,12 +1,28 @@
 // Задание 4.1. Вывод карточек товаров
 
+
+
+
 function parseProducts(json) {
+    let data;
+    data = JSON.parse(json);
+    let products;
+    products = data.products;
+    return products;
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+}
+function renderProductsCards(json) {
+    function clearProducts();
+    let products = parseProducts(json);
+    length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);
+    }
 }
 
 // Напишите функцию renderProductsCards(json)
